e040d110126bf19342781d395528e5f995bed821
koo <ninewrk@gmail.com>
2025-11-05 18:08:09 +0900

    Modify main and productpage

--- js/product-detail-cut.js
+++ js/product-detail-cut.js
@@ -3,7 +3,7 @@
 
 // ===== PERFUME (20) =====
 const detailList = [
-{
+{ Id:"0",
   brand: "Maison Margiela",
   name: "메종 마르지엘라 레플리카 레이지 선데이 모닝 오 드 뚜왈렛",
   category: "PERFUME",
@@ -17,7 +17,7 @@ const detailList = [
   ],
   info: `품명 : 메종마르지엘라 레이지 선데이 모닝 오 드 뚜왈렛전성분 : 에탄올, 향료, 정제수, 부틸메톡시디벤조일메탄, 에칠헥실메톡시신나메이트, 자색401호, 황색4호, 벤질알코올, 벤질살리실레이트, 리날룰, 시트로넬올, 제라니올, 신나밀알코올, 하이드록시시트로넬알, 파네솔, 리모넨*제공된 성분은 동일 제품이라도 경우에 따라 변경 될 수 있습니다**최신 정보는 제품의 포장을 참고*용량 : 30ml /100ml제품주요사항 : 모든 피부 타입사용방법 : 맥박이 뛰는 부위에 분사하여 사용합니다.사용기한 : 개봉 후 3년제조자 : 메종마르지엘라제조국 : 프랑스사용할 때의 주의사항 : 1. 화장품 사용 시 또는 사용 후 직사광선에 의하여 사용부위가 붉은 반점, 부어오름 또는 가려움증 등의 이상 증상이나부작용이 있는 경우 전문의 등과 상담할 것 2. 상처가 있는 부위 등에는 사용을 자제할 것 3. 보관 및 취급 시의 주의사항1) 어린이의 손이 닿지 않는 곳에 보관할 것 2) 직사광선을 피해서 보관할 것 4. 눈에 들어가지 않도록 주의하고 눈에 들어간 때에는 즉시 씻어 낼 것5.민감성 피부 보유자는 귓볼 등에 사전 패치 테스트 후 사용할 것품질보증기준 : 본 제품에 이상이 있을 경우, 공정거래위원회 고시 소비자분쟁해결기준에 의해 보상해 드립니다.`,
   },
-{
+{ Id:"1",
   brand: "Diptyque",
   name: "딥티크 오르페옹 오 드 퍼퓸",
   category: "PERFUME",
@@ -31,24 +31,27 @@ const detailList = [
   ],
   info: `품명 : 딥티크 오르페옹 오 드 퍼퓸전성분 : 에탄올, 향료, 정제수, 부틸메톡시디벤조일메탄, 에칠헥실메톡시신나메이트, 자색401호, 황색4호, 벤질알코올, 벤질살리실레이트, 리날룰, 시트로넬올, 제라니올, 신나밀알코올, 하이드록시시트로넬알, 파네솔, 리모넨*제공된 성분은 동일 제품이라도 경우에 따라 변경 될 수 있습니다**최신 정보는 제품의 포장을 참고*용량 : 30ml /100ml제품주요사항 : 모든 피부 타입사용방법 : 맥박이 뛰는 부위에 분사하여 사용합니다.사용기한 : 개봉 후 3년제조자 : 딥디크제조국 : 프랑스사용할 때의 주의사항 : 1. 화장품 사용 시 또는 사용 후 직사광선에 의하여 사용부위가 붉은 반점, 부어오름 또는 가려움증 등의 이상 증상이나부작용이 있는 경우 전문의 등과 상담할 것 2. 상처가 있는 부위 등에는 사용을 자제할 것 3. 보관 및 취급 시의 주의사항1) 어린이의 손이 닿지 않는 곳에 보관할 것 2) 직사광선을 피해서 보관할 것 4. 눈에 들어가지 않도록 주의하고 눈에 들어간 때에는 즉시 씻어 낼 것5.민감성 피부 보유자는 귓볼 등에 사전 패치 테스트 후 사용할 것품질보증기준 : 본 제품에 이상이 있을 경우, 공정거래위원회 고시 소비자분쟁해결기준에 의해 보상해 드립니다.`,
 },
-{
+{ Id:"2",
   brand: "Lanvin",
   name: "랑방 에끌라 드 아르페쥬 오 드 퍼퓸",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst3-lanvin-eclat-darpege-eau-de-parfum.jpg",
+  detailImages:"./img/detail-image/detailpage3.jpg",
   tags: { scent: "플로랄", mood: "로맨틱한", lasting: "가볍고 은은하게" },
   rating: 4.58,
   variants: [
     { size: "100ml", price: 56000 },
     { size: "50ml",  price: 39000 },
     { size: "30ml",  price: 28000 }
-  ]
+  ],
+  info:`품명 : 랑방 에끌라 드 아르페쥬 EDP 전성분 : 변성알코올39C, 향료, 물, 에칠헥실메톡시신나메이트, 부틸메톡시디벤조일메탄, 에칠헥실살리실레이트, 비에이치티, 하이드록시이소헥실3-사이클로헥센카복스알데하이드, 부틸페닐메칠프로피오날, 리모넨, 시트랄, 리닐룰, 제라니올, 자색401호, 적색227호*제공된 성분은 동일 제품이라도 경우에 따라 변경 될 수 있습니다**최신 정보는 제품의 포장을 참고*용량 : 30ml /50ml/100ml제품주요사항 : 모든 피부 타입사용방법 : 맥박이 뛰는 부위에 분사하여 사용합니다.사용기한 : 개봉 후 3년제조자 : 인터퍼퓸프랑스 / 라운드뷰티월드,다현상사,아미코즈,인터코스메틱,제이에스티제조국 : 프랑스사용할 때의 주의사항 : 1. 화장품 사용 시 또는 사용 후 직사광선에 의하여 사용부위가 붉은 반점, 부어오름 또는 가려움증 등의 이상 증상이나부작용이 있는 경우 전문의 등과 상담할 것 2. 상처가 있는 부위 등에는 사용을 자제할 것 3. 보관 및 취급 시의 주의사항1) 어린이의 손이 닿지 않는 곳에 보관할 것 2) 직사광선을 피해서 보관할 것 4. 눈에 들어가지 않도록 주의하고 눈에 들어간 때에는 즉시 씻어 낼 것5.민감성 피부 보유자는 귓볼 등에 사전 패치 테스트 후 사용할 것품질보증기준 : 본 제품에 이상이 있을 경우, 공정거래위원회 고시 소비자분쟁해결기준에 의해 보상해 드립니다.`
 },
-{
+{ Id:"3",
   brand: "Jennifer Lopez",
   name: "제니퍼로페즈 글로우 바이제이로 EDT",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst4-jenniferlopez-glow-by-jlo-edt.jpg",
+  detailImages:"./img/detail-image/detailpage4.jpg",
   tags: { scent: "프레시", mood: "에너제틱한", lasting: "가볍고 은은하게" },
   rating: 4.56,
   variants: [
@@ -57,33 +60,36 @@ const detailList = [
     { size: "30ml",  price: 55000 }
   ]
 },
-{
+{ Id:"4",
   brand: "Santa Maria Novella",
   name: "산타 마리아 노벨라 피렌체 1221 에디션 오 드 코롱",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst5-santamarianovella-pfirenze1221-edt.jpg",
+  detailImages:"./img/detail-image/detailpage5.jpg",
   tags: { scent: "오리엔탈", mood: "자신감있는", lasting: "오래가고 진하게" },
   rating: 4.48,
   variants: [
     { size: "100ml", price: 190000 }
   ]
 },
-{
+{ Id:"5",
   brand: "Daniel's Truth",
   name: "다니엘 트루스 오일 퍼퓸 밤쉘",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst6-danielstruth-oil-perfume-babel.jpg",
+  detailImages:"./img/detail-image/detailpage6.jpg",
   tags: { scent: "우디", mood: "편안함", lasting: "오래가고 진하게" },
   rating: 4.44,
   variants: [
     { size: "10ml", price: 59000 }
   ]
 },
-{
+{ Id:"6",
   brand: "Diptyque",
   name: "딥티크 플레르 드 뽀 오 드 퍼퓸",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst7-diptyque-fleur-depeau-eau-de-parfum.jpg",
+  detailImages:"./img/detail-image/detailpage7.jpg",
   tags: { scent: "기타", mood: "편안함", lasting: "가볍고 은은하게" },
   rating: 4.38,
   variants: [
@@ -91,11 +97,12 @@ const detailList = [
     { size: "125ml", price: 388000 }
   ]
 },
-{
+{ Id:"7",
   brand: "Jo Malone",
   name: "조 말론 블랙베리 앤 베이 코롱",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst8-jomalone-blackberry-and-bay-cologne.jpg",
+  detailImages:"./img/detail-image/detailpage8.jpg",
   tags: { scent: "프레시", mood: "에너제틱한", lasting: "가볍고 은은하게" },
   rating: 4.32,
   variants: [
@@ -105,11 +112,12 @@ const detailList = [
     { size: "100ml", price: 239000 }
   ]
 },
-{
+{ Id:"8",
   brand: "Clean",
   name: "클린 웜 코튼 EDP",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst9-clean-warm-cotton-edp.jpg",
+  detailImages:"./img/detail-image/detailpage9.jpg",
   tags: { scent: "프레시", mood: "편안함", lasting: "가볍고 은은하게" },
   rating: 4.22,
   variants: [
@@ -117,11 +125,12 @@ const detailList = [
     { size: "100ml", price: 160000 }
   ]
 },
-{
+{ Id:"9",
   brand: "Byredo",
   name: "바이레도 블랑쉬 오 드 퍼퓸",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst10-byredo-blanche-eau-de-parfum.jpg",
+  detailImages:"./img/detail-image/detailpage10.jpg",
   tags: { scent: "플로랄", mood: "편안함", lasting: "가볍고 은은하게" },
   rating: 4.16,
   variants: [
@@ -129,11 +138,12 @@ const detailList = [
     { size: "100ml", price: 390000 }
   ]
 },
-{
+{ Id:"10",
   brand: "Le Labo",
   name: "르라보 상탈 33 오 드 퍼퓸",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst11-lelabo-santal33.jpg",
+  detailImages:"./img/detail-image/detailpage11.jpg",
   tags: { scent: "우디", mood: "자신감있는", lasting: "오래가고 진하게" },
   rating: 4.88,
   variants: [
@@ -147,6 +157,7 @@ const detailList = [
   name: "르라보 어나더 13 오 드 퍼퓸",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst12-lelabo-another13.jpg",
+  detailImages:"./img/detail-image/detailpage12.jpg",
   tags: { scent: "기타", mood: "편안함", lasting: "오래가고 진하게" },
   rating: 4.85,
   variants: [
@@ -155,11 +166,12 @@ const detailList = [
     { size: "100ml", price: 458000 }
   ]
 },
-{
+{ Id:"12",
   brand: "Creed",
   name: "크리드 어벤투스 오 드 퍼퓸",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst13-creed-aventus.jpg",
+  detailImages:"./img/detail-image/detailpage13.jpg",
   tags: { scent: "우디", mood: "자신감있는", lasting: "오래가고 진하게" },
   rating: 4.83,
   variants: [
@@ -167,11 +179,12 @@ const detailList = [
     { size: "100ml", price: 520000 }
   ]
 },
-{
+{ 
   brand: "Byredo",
   name: "바이레도 집시 워터 오 드 퍼퓸",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst14-byredo-gypsywater.jpg",
+  detailImages:"./img/detail-image/detailpage14.jpg",
   tags: { scent: "우디", mood: "신비로운", lasting: "가볍고 은은하게" },
   rating: 4.78,
   variants: [
@@ -184,6 +197,7 @@ const detailList = [
   name: "톰 포드 오드 우드 오 드 퍼퓸",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst15-tomford-oudwood.jpg",
+  detailImages:"./img/detail-image/detailpage15.jpg",
   tags: { scent: "오리엔탈", mood: "자신감있는", lasting: "오래가고 진하게" },
   rating: 4.77,
   variants: [
@@ -197,6 +211,7 @@ const detailList = [
   name: "바이레도 모하비 고스트 오 드 퍼퓸",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst16-byredo-mojaveghost.jpg",
+  detailImages:"./img/detail-image/detailpage16.jpg",
   tags: { scent: "플로랄", mood: "자신감있는", lasting: "가볍고 은은하게" },
   rating: 4.75,
   variants: [
@@ -209,6 +224,7 @@ const detailList = [
   name: "바이레도 발 다프리크 오 드 퍼퓸",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst17-byredo-baldafrique.jpg",
+  detailImages:"./img/detail-image/detailpage17.jpg",
   tags: { scent: "프레시", mood: "에너제틱한", lasting: "가볍고 은은하게" },
   rating: 4.73,
   variants: [
@@ -221,6 +237,7 @@ const detailList = [
   name: "디올 소바쥬 오 드 뚜왈렛",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst18-dior-sauvage.jpg",
+  detailImages:"./img/detail-image/detailpage18.jpg",
   tags: { scent: "프레시", mood: "에너제틱한", lasting: "오래가고 진하게" },
   rating: 4.71,
   variants: [
@@ -234,6 +251,7 @@ const detailList = [
   name: "톰 포드 블랙 오키드 오 드 퍼퓸",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst19-tomford-blackorchid.jpg",
+  detailImages:"./img/detail-image/detailpage19.jpg",
   tags: { scent: "오리엔탈", mood: "신비로운", lasting: "오래가고 진하게" },
   rating: 4.66,
   variants: [
@@ -247,6 +265,7 @@ const detailList = [
   name: "킬리안 엔젤스 셰어",
   category: "PERFUME",
   image: "./img/product-list/rank-l/perfume/lst20-kilian-angelsshare.jpg",
+  detailImages:"./img/detail-image/detailpage20.jpg",
   tags: { scent: "오리엔탈", mood: "로맨틱한", lasting: "오래가고 진하게" },
   rating: 4.64,
   variants: [
@@ -260,6 +279,7 @@ const detailList = [
   name: "르라보 어나더 13 퍼퓨밍 바디 로션",
   category: "BODY",
   image: "./img/product-list/rank-l/body/lst1-lelabo-bodylotion-another13.jpg",
+  detailImages:"./img/detail-image/detailpage21.jpg",
   tags: { scent: "기타", mood: "편안함", lasting: "가볍고 은은하게" },
   rating: 4.83,
   variants: [
@@ -271,6 +291,7 @@ const detailList = [
   name: "바이레도 모하비 고스트 핸드 크림",
   category: "BODY",
   image: "./img/product-list/rank-l/body/lst2-byredo-handcream-mojaveghost.jpg",
+  detailImages:"./img/detail-image/detailpage22.jpg",
   tags: { scent: "플로랄", mood: "자신감있는", lasting: "가볍고 은은하게" },
   rating: 4.76,
   variants: [
@@ -282,6 +303,7 @@ const detailList = [
   name: "바이레도 블랑쉬 핸드 크림",
   category: "BODY",
   image: "./img/product-list/rank-l/body/lst3-byredo-handcream-blanche.jpg",
+  detailImages:"./img/detail-image/detailpage23.jpg",
   tags: { scent: "플로랄", mood: "편안함", lasting: "가볍고 은은하게" },
   rating: 4.74,
   variants: [
@@ -293,6 +315,7 @@ const detailList = [
   name: "이솝 레저렉션 아로마틱 핸드 밤",
   category: "BODY",
   image: "./img/product-list/rank-l/body/lst4-aesop-resurrection-handbalm.jpg",
+  detailImages:"./img/detail-image/detailpage24.jpg",
   tags: { scent: "프레시", mood: "편안함", lasting: "가볍고 은은하게" },
   rating: 4.69,
   variants: [
@@ -304,6 +327,7 @@ const detailList = [
   name: "바이레도 발 다프리크 바디 로션",
   category: "BODY",
   image: "./img/product-list/rank-l/body/lst5-byredo-bodylotion-baldaffrique.jpg",
+  detailImages:"./img/detail-image/detailpage25.jpg",
   tags: { scent: "프레시", mood: "에너제틱한", lasting: "가볍고 은은하게" },
   rating: 4.69,
   variants: [
@@ -315,6 +339,7 @@ const detailList = [
   name: "딥티크 핸드 & 바디 로션 플레르 드 뽀",
   category: "BODY",
   image: "./img/product-list/rank-l/body/lst6-diptyque-bodylotion-fleurdepeau.jpg",
+  detailImages:"./img/detail-image/detailpage26.jpg",
   tags: { scent: "기타", mood: "편안함", lasting: "가볍고 은은하게" },
   rating: 4.68,
   variants: [
@@ -326,6 +351,7 @@ const detailList = [
   name: "디올 뉴 디올 르 밤 핸드 크림",
   category: "BODY",
   image: "./img/product-list/rank-l/body/lst7-dior-handcream-lebaume.jpg",
+  detailImages:"./img/detail-image/detailpage27.jpg",
   tags: { scent: "프레시", mood: "편안함", lasting: "가볍고 은은하게" },
   rating: 4.67,
   variants: [
@@ -337,6 +363,7 @@ const detailList = [
   name: "이솝 레저렉션 아로마틱 핸드 워시",
   category: "BODY",
   image: "./img/product-list/rank-l/body/lst8-aesop-resurrection-handwash.jpg",
+  detailImages:"./img/detail-image/detailpage28.jpg",
   tags: { scent: "프레시", mood: "에너제틱한", lasting: "가볍고 은은하게" },
   rating: 4.67,
   variants: [
@@ -348,6 +375,7 @@ const detailList = [
   name: "르라보 핸드 포마드 히노키",
   category: "BODY",
   image: "./img/product-list/rank-l/body/lst9-lelabo-handpomade-hinoki.jpg",
+  detailImages:"./img/detail-image/detailpage29.jpg",
   tags: { scent: "우디", mood: "편안함", lasting: "가볍고 은은하게" },
   rating: 4.63,
   variants: [
@@ -359,6 +387,7 @@ const detailList = [
   name: "딥티크 퍼퓸 핸드 크림 도 손",
   category: "BODY",
   image: "./img/product-list/rank-l/body/lst10-diptyque-handcream-doson.jpg",
+  detailImages:"./img/detail-image/detailpage30.jpg",
   tags: { scent: "플로랄", mood: "로맨틱한", lasting: "가볍고 은은하게" },
   rating: 4.63,
   variants: [
@@ -575,33 +604,36 @@ const detailList = [
   ]
 },
 // ===== new PERFUME (6) — rating 없음 → "-" =====
-{
+{ Id:"49",
   brand: "Tamburins",
   name: "탬버린즈 퍼퓸 리미티드 퍼피",
   category: "PERFUME",
   image: "./img/product-list/new-detail/perfume/new1-tamburins-limited-puppy.jpg",
+  detailImages: "./img/detail-image/new01.jpg",
   tags: { scent: "오리엔탈", mood: "자신감있는", lasting: "오래가고 진하게" },
   rating: "-",
   variants: [
     { size: "50ml", price: 250000 }
   ]
 },
-{
+{ Id:"50",
   brand: "Tamburins",
   name: "탬버린즈 퍼퓸 리미티드 선샤인",
   category: "PERFUME",
   image: "./img/product-list/new-detail/perfume/new2-tamburins-limited-sunshine.jpg",
+  detailImages: "./img/detail-image/new02.jpg",
   tags: { scent: "프레시", mood: "에너제틱한", lasting: "가볍고 은은하게" },
   rating: "-",
   variants: [
     { size: "50ml", price: 250000 }
   ]
 },
-{
+{ Id:"51",
   brand: "Byredo",
   name: "바이레도 알토 아스트랄 오 드 퍼퓸",
   category: "PERFUME",
   image: "./img/product-list/new-detail/perfume/new3-byredo-astral-oud.jpg",
+  detailImages: "./img/detail-image/new03.jpg",
   tags: { scent: "오리엔탈", mood: "자신감있는", lasting: "오래가고 진하게" },
   rating: "-",
   variants: [
@@ -609,11 +641,12 @@ const detailList = [
     { size: "100ml", price: 495000 }
   ]
 },
-{
+{ Id:"52",
   brand: "Yves Saint",
   name: "입생로랑 베이비캣 오 드 빠르펭",
   category: "PERFUME",
   image: "./img/product-list/new-detail/perfume/new4-yves-saint-laurent-babycat.jpg",
+  detailImages: "./img/detail-image/new04.jpg",
   tags: { scent: "오리엔탈", mood: "자신감있는", lasting: "오래가고 진하게" },
   rating: "-",
   variants: [
@@ -621,11 +654,12 @@ const detailList = [
     { size: "125ml", price: 540000 }
   ]
 },
-{
+{ Id:"53",
   brand: "Diptyque",
   name: "딥티크 플레르 드 뽀 오 드 뚜왈렛",
   category: "PERFUME",
   image: "./img/product-list/new-detail/perfume/new5-diptyque-fleur-de-peau.jpg",
+  detailImages: "./img/detail-image/new05.jpg",
   tags: { scent: "기타", mood: "편안함", lasting: "가볍고 은은하게" },
   rating: "-",
   variants: [
@@ -633,11 +667,12 @@ const detailList = [
     { size: "125ml", price: 318000 }
   ]
 },
-{
+{ Id:"54",
   brand: "Le Labo",
   name: "르라보 오스만투스 19",
   category: "PERFUME",
   image: "./img/product-list/new-detail/perfume/new6-le-labo-osmanthus19.jpg",
+  detailImages: "./img/detail-image/new06.jpg",
   tags: { scent: "플로랄", mood: "로맨틱한", lasting: "가볍고 은은하게" },
   rating: "-",
   variants: [
@@ -648,7 +683,7 @@ const detailList = [
 },
 
 // ===== BODY =====
-{
+{ Id:"55",
   brand: "Byredo",
   name: "바이레도 유자 트리 핸드 로션",
   category: "BODY",
@@ -659,7 +694,7 @@ const detailList = [
     { size: "450ml", price: 79000 }
   ]
 },
-{
+{ Id:"56",
   brand: "Acqua Di Parma",
   name: "아쿠아 디 파르마 핸드 앤 바디 로션 부옹조르노",
   category: "BODY",
@@ -670,7 +705,7 @@ const detailList = [
     { size: "300ml", price: 105000 }
   ]
 },
-{
+{ Id:"57",
   brand: "Jo Malone",
   name: "조말론 라임 바실 앤 만다린 바디 앤 핸드 로션",
   category: "BODY",
@@ -681,7 +716,7 @@ const detailList = [
     { size: "250ml", price: 115000 }
   ]
 },
-{
+{ Id:"58",
   brand: "Diptyque",
   name: "딥디크 프레쉬 로션 포 더 바디",
   category: "BODY",
@@ -692,7 +727,7 @@ const detailList = [
     { size: "200ml", price: 89000 }
   ]
 },
-{
+{ Id:"59",
   brand: "Diptyque",
   name: "딥디크 플레르 드 포 핸드 앤 바디 로션",
   category: "BODY",
@@ -703,7 +738,7 @@ const detailList = [
     { size: "350ml", price: 95000 }
   ]
 },
-{
+{ Id:"60",
   brand: "Tamburins",
   name: "탬버린즈 샤워리 바디 로션 포그",
   category: "BODY",
@@ -714,7 +749,7 @@ const detailList = [
     { size: "250ml", price: 48000 }
   ]
 },
-{
+{ Id:"61",
   brand: "Tamburins",
   name: "탬버린즈 샤워리 바디 로션 님버스",
   category: "BODY",
@@ -727,7 +762,7 @@ const detailList = [
 },
 
 // ===== HAIR =====
-{
+{ Id:"62",
   brand: "Diptyque",
   name: "딥티크 리미티드 에디션 퍼퓸 샴푸 포 헤어",
   category: "HAIR",
@@ -738,7 +773,7 @@ const detailList = [
     { size: "195ml", price: 87000 }
   ]
 },
-{
+{ Id:"63",
   brand: "Diptyque",
   name: "딥티크 필로시코스 헤어 미스트",
   category: "HAIR",
@@ -749,7 +784,7 @@ const detailList = [
     { size: "30ml", price: 94000 }
   ]
 },
-{
+{ Id:"64",
   brand: "Diptyque",
   name: "딥티크 일리오 리미티드 에디션 헤어 미스트",
   category: "HAIR",
@@ -760,7 +795,7 @@ const detailList = [
     { size: "30ml", price: 93000 }
   ]
 },
-{
+{ Id:"65",
   brand: "Prada",
   name: "프라다 패러독스 헤어 미스트",
   category: "HAIR",
@@ -771,7 +806,7 @@ const detailList = [
     { size: "30ml", price: 72000 }
   ]
 },
-{
+{ Id:"66",
   brand: "Dior",
   name: "디올 미스 디올 오 드 퍼퓸 헤어 미스트",
   category: "HAIR",
@@ -784,7 +819,7 @@ const detailList = [
 },
 
 // ===== ETC =====
-{
+{ Id:"67",
   brand: "Le Labo",
   name: "르라보 페티 그레인 21 홈 프래그런스",
   category: "ETC",
@@ -795,7 +830,7 @@ const detailList = [
     { size: "100ml", price: 169000 }
   ]
 },
-{
+{ Id:"68",
   brand: "Nonfiction",
   name: "논픽션 랍상송 룸 스프레이",
   category: "ETC",
@@ -807,7 +842,7 @@ const detailList = [
     { size: "150ml", price: 65000 }
   ]
 },
-{
+{ Id:"69",
   brand: "Diptyque",
   name: "딥티크 리미티드 에디션 클래식 시트로넬 캔들",
   category: "ETC",
@@ -818,7 +853,7 @@ const detailList = [
     { size: "190g", price: 108000 }
   ]
 },
-{
+{ Id:"70",
   brand: "Aesop",
   name: "이솝 무라사키 아로마틱 인센스",
   category: "ETC",
@@ -829,7 +864,7 @@ const detailList = [
     { size: "33 sticks (13g)", price: 47000 }
   ]
 },
-{
+{ Id:"71",
   brand: "Aesop",
   name: "이솝 이스트로스 콤팩트 룸 스프레이",
   category: "ETC",
@@ -840,7 +875,7 @@ const detailList = [
     { size: "50ml", price: 49000 }
   ]
 },
-{
+{ Id:"72",
   brand: "Aesop",
   name: "이솝 키테라 콤팩트 룸 스프레이",
   category: "ETC",
@@ -851,7 +886,7 @@ const detailList = [
     { size: "50ml", price: 60000 }
   ]
 },
-{
+{ Id:"73",
   brand: "Aesop",
   name: "이솝 올루스 콤팩트 룸 스프레이",
   category: "ETC",
